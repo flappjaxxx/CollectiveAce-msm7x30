ecb664618a406ca2faee05d7e5f4db0a4646a77d
Laurence <flappjaxxx@gmail.com>
2012-02-05 12:26:57 -0500

    updated to version 0.1.0 AbsolutionACE

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 # Created by eXistZ
 
 
-VERSION="v1.1.3"
-KERNEL_SRC="/usr/src/htc-kernel-msm7x30"
+VERSION="v0.1.0"
+KERNEL_SRC="/home/ctindall/existz/htc-kernel-msm7x30"
 
 # Linaro Android 4.5 (GCC 4.5.4) toolchain - http://www.linaro.org
-export CROSS_COMPILE="/opt/toolchains/android-toolchain-eabi-4.5/bin/arm-eabi-"
+export CROSS_COMPILE="/home/ctindall/toolchain/bin/arm-none-eabi-"
 
 export ARCH=arm
-export LOCALVERSION="-TalonACE_7x30-$VERSION"
+export LOCALVERSION="-AbsolutionACE_ICS-$VERSION"
 
 START=$(date +%s)
 
-make talon_msm7230_defconfig
+make absoace_defconfig
 
 if [ -e ./releasetools/system/lib/modules ]; then
  rm -rf ./releasetools/system/lib/modules
@@ -35,7 +35,7 @@ make -j`grep 'processor' /proc/cpuinfo | wc -l`
 cp $KERNEL_SRC/arch/arm/boot/zImage $KERNEL_SRC/releasetools/kernel/
 cd $KERNEL_SRC/releasetools
 rm -f *.zip
-zip -r TalonACE_7x30-$VERSION.zip *
+zip -r AbsolutionACE_ICS-$VERSION.zip *
 rm $KERNEL_SRC/releasetools/kernel/zImage
 
 cd $KERNEL_SRC
--- drivers/cpufreq/cpufreq_brazilianwax.c
+++ drivers/cpufreq/cpufreq_brazilianwax.c
@@ -86,7 +86,7 @@ static unsigned long down_rate_us;
  * When ramping up frequency with no idle cycles jump to at least this frequency.
  * Zero disables. Set a very high value to jump to policy max freqeuncy.
  */
-#define DEFAULT_UP_MIN_FREQ 1881600
+#define DEFAULT_UP_MIN_FREQ 1843200
 static unsigned int up_min_freq;
 
 /*
@@ -95,17 +95,17 @@ static unsigned int up_min_freq;
  * to minimize wakeup issues.
  * Set sleep_max_freq=0 to disable this behavior.
  */
-#define DEFAULT_SLEEP_MAX_FREQ 345600
+#define DEFAULT_SLEEP_MAX_FREQ 384000
 static unsigned int sleep_max_freq;
 
 /*
  * The frequency to set when waking up from sleep.
  * When sleep_max_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 960000
+#define DEFAULT_SLEEP_WAKEUP_FREQ 921600
 static unsigned int sleep_wakeup_freq;
 
-#define UP_THRESHOLD_FREQ 1804800
+#define UP_THRESHOLD_FREQ 1843200
 static unsigned int threshold_freq;
 
 /*
@@ -113,10 +113,10 @@ static unsigned int threshold_freq;
  * go below this frequency.
  * Set awake_min_freq=0 to disable this behavior.
  */
-#define DEFAULT_AWAKE_MIN_FREQ 192000
+#define DEFAULT_AWAKE_MIN_FREQ 122000
 static unsigned int awake_min_freq;
 
-static unsigned int suspendfreq = 345600;
+static unsigned int suspendfreq = 384000;
 
 /*
  * Sampling rate, I highly recommend to leave it at 2.
@@ -128,14 +128,14 @@ static unsigned int sample_rate_jiffies;
  * Minimum Freqeuncy delta when ramping up.
  * zero disables and causes to always jump straight to max frequency.
  */
-#define DEFAULT_RAMP_UP_STEP 422400
+#define DEFAULT_RAMP_UP_STEP 460800
 static unsigned int ramp_up_step;
 
 /*
  * Miminum Freqeuncy delta when ramping down.
  * zero disables and will calculate ramp down according to load heuristic.
  */
-#define DEFAULT_RAMP_DOWN_STEP 345600
+#define DEFAULT_RAMP_DOWN_STEP 384000
 static unsigned int ramp_down_step;
 
 /*
